c5f2ae89b929751095f43bfb7bed0a40f2cecffa
Chris Kenyon <Chris Kenyon>
2017-01-25 15:37:25 -0500

    Finished int/ and round

--- hw02.lisp
+++ hw02.lisp
@@ -1,3 +1,61 @@
+; **************** BEGIN INITIALIZATION FOR ACL2s B MODE ****************** ;
+; (Nothing to see here!  Your actual file is after this initialization code);
+
+#|
+Pete Manolios
+Fri Jan 27 09:39:00 EST 2012
+----------------------------
+
+Made changes for spring 2012.
+
+
+Pete Manolios
+Thu Jan 27 18:53:33 EST 2011
+----------------------------
+
+The Beginner level is the next level after Bare Bones level.
+
+|#
+
+; Put CCG book first in order, since it seems this results in faster loading of this mode.
+#+acl2s-startup (er-progn (assign fmt-error-msg "Problem loading the CCG book.~%Please choose \"Recertify ACL2s system books\" under the ACL2s menu and retry after successful recertification.") (value :invisible))
+(include-book "ccg/ccg" :uncertified-okp nil :dir :acl2s-modes :ttags ((:ccg)) :load-compiled-file nil);v4.0 change
+
+;Common base theory for all modes.
+#+acl2s-startup (er-progn (assign fmt-error-msg "Problem loading ACL2s base theory book.~%Please choose \"Recertify ACL2s system books\" under the ACL2s menu and retry after successful recertification.") (value :invisible))
+(include-book "base-theory" :dir :acl2s-modes)
+
+#+acl2s-startup (er-progn (assign fmt-error-msg "Problem loading ACL2s customizations book.~%Please choose \"Recertify ACL2s system books\" under the ACL2s menu and retry after successful recertification.") (value :invisible))
+(include-book "custom" :dir :acl2s-modes :uncertified-okp nil :ttags :all)
+
+;Settings common to all ACL2s modes
+(acl2s-common-settings)
+
+#+acl2s-startup (er-progn (assign fmt-error-msg "Problem loading trace-star and evalable-ld-printing books.~%Please choose \"Recertify ACL2s system books\" under the ACL2s menu and retry after successful recertification.") (value :invisible))
+(include-book "trace-star" :uncertified-okp nil :dir :acl2s-modes :ttags ((:acl2s-interaction)) :load-compiled-file nil)
+(include-book "hacking/evalable-ld-printing" :uncertified-okp nil :dir :system :ttags ((:evalable-ld-printing)) :load-compiled-file nil)
+
+;theory for beginner mode
+#+acl2s-startup (er-progn (assign fmt-error-msg "Problem loading ACL2s beginner theory book.~%Please choose \"Recertify ACL2s system books\" under the ACL2s menu and retry after successful recertification.") (value :invisible))
+(include-book "beginner-theory" :dir :acl2s-modes :ttags :all)
+
+
+#+acl2s-startup (er-progn (assign fmt-error-msg "Problem setting up ACL2s Beginner mode.") (value :invisible))
+;Settings specific to ACL2s Beginner mode.
+(acl2s-beginner-settings)
+
+; why why why why 
+(acl2::xdoc acl2s::defunc) ; almost 3 seconds
+
+(cw "~@0Beginner mode loaded.~%~@1"
+    #+acl2s-startup "${NoMoReSnIp}$~%" #-acl2s-startup ""
+    #+acl2s-startup "${SnIpMeHeRe}$~%" #-acl2s-startup "")
+
+
+(acl2::in-package "ACL2S B")
+
+; ***************** END INITIALIZATION FOR ACL2s B MODE ******************* ;
+;$ACL2s-SMode$;Beginner
 #|
 
 CS 2800 Homework 2 - Spring 2017
@@ -480,6 +538,16 @@ EX: (check= (- 4/3 1) 1/3)
 ;; positive and rounded UP if the result is negative.
 ;; HINT: Do not re-invent the wheel here. Use previous functions.
 (defunc int/ (x y)
+  :input-contract (and (integerp x)(integerp y)(not (equal 0 y)))
+  :output-contract (integerp (int/ x y))
+  (let* ((x-neg (< 0 x))
+        (y-neg (< 0 y))
+        (neg (and (or x-neg y-neg)(not (and x-neg y-neg)))) ;xor
+        (posdiv (nat/ (abs x) (abs y))))
+        (if neg
+          (unary-- posdiv)
+          posdiv
+        )))
  ; .......
   
   
@@ -527,9 +595,23 @@ EX: (check= (- 4/3 1) 1/3)
 ;; and rounded down for values of Y <= 1/2
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defunc round (r)
+  :input-contract (rationalp r)
+  :output-contract (integerp (round r))
+  (let* ((absnum (abs (numerator r)))
+         (absden (abs (denominator r)))
+         (posfloor (nat/ absnum absden))
+         (remhalf (/ (rem absnum absden) absden))
+         (rounded (if (> remhalf 1/2) (+ posfloor 1) posfloor)))
+    
+         (cond ((integerp r)            r)
+           ((< (numerator r) 0)         (unary-- rounded))
+           (t                           rounded))))
+
  ; ........
   
 (check= (round 4/3) 1)
 (check= (round -4/3) -1)
 (check= (round 5/3) 2)
 (check= (round -5/3) -2)
+(check= (round 0) 0)
+(check= (round 10) 10)#|ACL2s-ToDo-Line|#
